94c1c8707939d28adc8a6f83565e34cb7b537fe9
Gallito <marifer.enriquezc@gmail.com>
2019-01-25 21:45:29 -0600

    Updates

---
+++ precache-manifest.8bddae44c2fe158a9a18b955a94c2c60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99756a16daaeda443979",
+    "url": "/CLICKY-GAME-BILLIONS/static/js/runtime~main.99756a16.js"
+  },
+  {
+    "revision": "773ed09d6b2ff67dd799",
+    "url": "/CLICKY-GAME-BILLIONS/static/js/main.773ed09d.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/CLICKY-GAME-BILLIONS/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "773ed09d6b2ff67dd799",
+    "url": "/CLICKY-GAME-BILLIONS/static/css/main.19cfbb0b.chunk.css"
+  },
+  {
+    "revision": "92cabc648a4a2d0c08318bc51e603697",
+    "url": "/CLICKY-GAME-BILLIONS/index.html"
+  }
+];
